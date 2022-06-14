32924022b4a43d839f503c661e5e7da0c6f54b29
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-06-14 15:07:57 +0530

    Resolved Database Issues

--- config/db.js
+++ config/db.js
@@ -1,73 +1,54 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const mysql = require("mysql2/promise");
 
-mysql
-  .createConnection({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-  })
-  // Raw Queries to only create a database if not exist
-  .then((connection) => {  
-    connection.query("CREATE DATABASE IF NOT EXISTS contacts");
-    connection.query("CREATE DATABASE IF NOT EXISTS global_users");
-    connection.query("CREATE DATABASE IF NOT EXISTS registered_users");
-  });
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.USER,
-  process.env.PASSWORD,
-  {
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
-    pool: { max: 5, min: 0, idle: 10000 },
-    logging: false
-  }
-);
-
-sequelize
-  .authenticate()
-  .then(function () {
+ const startUp =  async () => {
+  try {
+    const connection = await mysql.createConnection({
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+    });
+  
+    connection.query("CREATE DATABASE IF NOT EXISTS global_database");
+    const sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.USER,
+      process.env.PASSWORD,
+      {
+        host: process.env.HOST,
+        dialect: process.env.DIALECT,
+        pool: { max: 5, min: 0, idle: 10000 },
+        logging: false,
+      }
+    );
+  
+    await sequelize.authenticate();
     console.log("Connection Established Successfully");
-  })
-  .catch(function (err) {
-    console.log("Error : " + err);
-  });
-
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-
-var { registeredUsers } = require("../models/Registered_User");
-var { globalUsers } = require("../models/Global_User");
-var { contacts } = require("../models/Contacts");
-
-// Register User Table
-db.registeredUsers = registeredUsers(sequelize, DataTypes);
-
-// Global User Table
-db.globalUsers = globalUsers(sequelize, DataTypes);
-
-// Contact User Table
-db.contacts = contacts(sequelize, DataTypes);
-
-db.sequelize
-  .sync()
-  .then(function () {
+    const db = {};
+    db.Sequelize = Sequelize;
+    db.sequelize = sequelize;
+  
+    var { registeredUsers } = require("../models/Registered_User");
+    var { globalUsers } = require("../models/Global_User");
+    var { contacts } = require("../models/Contacts");
+  
+    // Register User Table
+    db.registeredUsers = registeredUsers(sequelize, DataTypes);
+  
+    // Global User Table
+    db.globalUsers = globalUsers(sequelize, DataTypes);
+  
+    // Contact User Table
+    db.contacts = contacts(sequelize, DataTypes);
+  
+    await db.sequelize.sync();
     console.log("Tables Created Successfully");
-  })
-  .catch(function (err) {
-    console.log("Error" + err);
-  });
+    
+    return db;
 
-// Association - One To Many Relation
+  } catch (error) {
+    console.log(error);
+  }
 
-db.registeredUsers.hasMany(db.contacts, {
-  foreignKey: "phoneNumber",
-});
+};
 
-db.contacts.belongsTo(db.registeredUsers, {
-  foreignKey: "phoneNumber",
-});
-
-module.exports = db;
+module.exports = startUp();
--- seeder.js
+++ seeder.js
@@ -1,57 +1,61 @@
 const fs = require("fs");
 const dotenv = require("dotenv");
-
 // Loading Env Variables
 dotenv.config({ path: "./config/config.env" });
 
 // Load Models
 
-const db = require("./config/db");
-const globalUsers = db.globalUsers;
-const contacts = db.contacts;
+const seeder = async () => {
+  const startUp = require("./config/db");
+  const db = await startUp;
 
-// Read JSON Files
-const registered_users = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/global_users.json`, "utf-8")
-);
+  const globalUsers = db.globalUsers;
+  const contacts = db.contacts;
 
-const contact_file = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/contacts.json`, "utf-8")
-);
+  // Read JSON Files
+  const registered_users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/global_users.json`, "utf-8")
+  );
 
-// Import into DB
-const importData = async function () {
-  try {
-    await globalUsers.bulkCreate(registered_users);
-    await contacts.bulkCreate(contact_file);
+  const contact_file = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/contacts.json`, "utf-8")
+  );
 
-    console.log("Data Imported...");
-    process.exit();
-  } catch (error) {
-    console.log(error);
+  // Import into DB
+  const importData = async function () {
+    try {
+      await globalUsers.bulkCreate(registered_users);
+      await contacts.bulkCreate(contact_file);
+      console.log("Data Imported...");
+      process.exit();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Delete Data
+  const deleteData = async function () {
+    try {
+      await globalUsers.destroy({
+        where: {},
+      });
+
+      await contacts.destroy({
+        where: {},
+      });
+
+      console.log("Data Destroyed...");
+      process.exit();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (process.argv[2] === "-i") {
+    importData();
+  } else if (process.argv[2] === "-d") {
+    deleteData();
   }
 };
 
-// Delete Data
-const deleteData = async function () {
-  try {
-    await globalUsers.destroy({
-      where: {},
-    });
-
-    await contacts.destroy({
-      where: {},
-    });
-
-    console.log("Data Destroyed...");
-    process.exit();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-if (process.argv[2] === "-i") {
-  importData();
-} else if (process.argv[2] === "-d") {
-  deleteData();
-}
+seeder();
